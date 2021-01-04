17ceda7f383f5729b14e3888d13962782e9644be
chiragthesia <harithesia@gmail.com>
2021-01-03 20:04:36 -0600

    Created primary applicaiton to interact with the backend

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//Component Import 
+import NavBar from "./components/NavBar"
+import Footer from "./components/Footer"
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <h1>Hello Welcome to the Journal!</h1>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Footer = ()=>{
+
+    return (
+    <footer>
+        <div>
+            <p>
+                All in 1 Journal Provided by Chirag Thesia!
+            </p>
+        </div>
+    </footer>)
+}
+
+export default Footer
